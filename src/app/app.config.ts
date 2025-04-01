124ccf4581f896ae6fcee0b571ca83482b54fa71
nayid2004@gmail.com <jmesquivel@unicesar.edu.co>
2025-04-01 10:07:21 -0500

    Ajuste de ver pelicula

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withDisabledInitialNavigation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -17,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(
       routes,
+      //withDisabledInitialNavigation(),
       withComponentInputBinding(),
       withInMemoryScrolling({
         anchorScrolling: 'enabled',
--- src/app/pages/invitado/ver-pelicula/ver-pelicula.component.ts
+++ src/app/pages/invitado/ver-pelicula/ver-pelicula.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PeliculaService } from '../../../core/services/pelicula.service';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, switchMap, takeUntil } from 'rxjs';
 import { Pelicula } from '../../../core/models/pelicula.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { TagModule } from 'primeng/tag';
 import { Tag } from 'primeng/tag';
@@ -17,13 +17,14 @@ import { Tag } from 'primeng/tag';
 })
 export class VerPeliculaComponent implements OnInit, OnDestroy {
 
-  pelicula!: Pelicula
+  pelicula: Pelicula | null = null;
   private unsubscribe$ = new Subject<void>();
-  safeTrailerUrl!: SafeResourceUrl;
+  safeTrailerUrl: SafeResourceUrl | null = null;
   
   constructor(
     private servicioPelicula: PeliculaService,
     private route: ActivatedRoute,
+    private router: Router,
     private sanitizer: DomSanitizer
   ) {}
 
@@ -33,22 +34,44 @@ export class VerPeliculaComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.route.params.pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
+      this.cargarPelicula();
+    });
+  }
 
-    const id = this.route.snapshot.paramMap.get('id');
-    if (!id) {
-      return;
+  cargarPelicula() {
+    this.route.params.pipe(
+      takeUntil(this.unsubscribe$),
+      switchMap(params => {
+        const id = params['id'];
+        
+        if (!id) {
+          this.router.navigate(['/']);
+          throw new Error('ID no proporcionado');
+        }
+
+        return this.servicioPelicula.ListarPorId(id);
+      })
+    ).subscribe({
+      next: (resp: Pelicula) => {
+        this.pelicula = resp;
+        this.safeTrailerUrl = this.getSafeTrailerUrl(resp.codigoDeTrailerEnYoutube);
+      },
     }
+    );
+  }
 
-    this.servicioPelicula
-      .ListarPorId(id)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe({
-        next: (resp: Pelicula) => {
-          this.pelicula = resp;
-          this.safeTrailerUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-            'https://www.youtube.com/embed/' + this.pelicula.codigoDeTrailerEnYoutube
-          );
-        },
-      });
+  private getSafeTrailerUrl(trailerCode: string): SafeResourceUrl {
+    if (!trailerCode) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('');
+    }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube.com/embed/${trailerCode}`
+    );
+  }
+
+  private resetState(): void {
+    this.pelicula = null;
+    this.safeTrailerUrl = null;
   }
 }
